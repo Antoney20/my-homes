e313c0dc08c8bd192c2b535ab37f028cbc611e06
ANTONY klerk <antomutwiri2001@gmail.com>
2023-07-24 13:53:58 +0300

    templates set up

--- myhome/views.py
+++ myhome/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+import requests
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'myhome/index.html')
+    
+def register(request):
+    return render(request, 'myhome/register.html')
+
+def confirm_email(request):
+    return render(request, 'myhome/register.html')
+    
+
+
